02243adf1b03206ee53f5e76de67651fce92ab67
AnthonyLaw <anthony@symbol.dev>
2024-07-25 14:22:31 +0800

    [explorer] fix: Refactor few transactions

--- __tests__/config/jest.setup.js
+++ __tests__/config/jest.setup.js
@@ -1,5 +1,8 @@
+
 /* Mock init http */
 jest.mock('../../src/infrastructure/http', () => {
+	const { Address } = require('symbol-sdk');
+
 	return {
 		networkType: 152,
 		epochAdjustment: 1615853185,
@@ -15,7 +18,8 @@ jest.mock('../../src/infrastructure/http', () => {
 			NetworkType: 152,
 			NemsisTimestamp: 1637848847,
 			NamespaceGraceDuration: 2880,
-			TotalChainImportance: 7842928625000000
+			TotalChainImportance: 7842928625000000,
+			MosaicRentalSinkAddress: Address.createFromRawAddress('TATYW7IJN2TDBINTESRU66HHS5HMC4YVW7GGDRA')
 		},
 		nativeNamespaces: [
 			{
